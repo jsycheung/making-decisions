b108a40431398facd1aaebea72622a6fb38889c2
Jasmine Cheung <jasminesyc2000@gmail.com>
2022-11-17 02:35:40 -0500

    Problem 2 solved

--- lab.js
+++ lab.js
@@ -28,6 +28,13 @@ var brittanisBirthYear = 1986;
 */
 
 //CODE HERE
+if (amysAge > brittanisAge) {
+  console.log("Amy is older.");
+} else if (brittanisAge > amysAge) {
+  console.log("Brittani is older.");
+} else {
+  console.log("Amy and Brittani are the same age.");
+}
 
 ////////// PROBLEM 3 //////////
 
